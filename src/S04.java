c7e374d82a733d8834399605494d8db425e3ea44
smileo5566 <liang40607@livemail.tw>
2017-09-26 01:02:15 +0800

    22

--- src/S04.java
+++ src/S04.java
@@ -1,24 +1,31 @@
 import java.util.Random;
 import java.util.Scanner;
 
+
 public class S04 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        Random ran = new Random();
-        int a = scn.nextInt();
-        ran.setSeed(a);
-        for(int i = 0; i <= 4; i++){
-            for(int j = 0; j <= 4; j++){
-                System.out.print(ran.nextInt(9)+"\t");
+        Random rd = new Random();
+        int b = scn.nextInt();
+        rd.setSeed(b);
+        int a[][] = new int[5][5];
+
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 0; j <= 4; j++) {
+                a[i][j] = rd.nextInt(10);
+                System.out.print(a[i][j] + "\t");
+
             }
             System.out.println();
         }
-        System.out.println("-------------------------");
-        for(int j = 0; j <= 4; j++){
-            for(int i = 0; i <= 4; i++){
-                System.out.print(ran.nextInt(9)+"\t");
+        System.out.println();
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 0; j <= 4; j++) {
+                System.out.print(a[j][i] + "\t");
+
             }
             System.out.println();
         }
+
     }
 }
--- src/TEST1.java
+++ src/TEST1.java
@@ -1,12 +1,31 @@
+import java.util.Random;
 import java.util.Scanner;
+
 
 public class TEST1 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        double f = scn.nextDouble();
-        double c = ((f/0.45359));
-        c =(int)(c*100);
-        c =c/100;
-        System.out.println(c);
+        Random rd = new Random();
+        int b = scn.nextInt();
+        rd.setSeed(b);
+        int a[][] = new int[5][5];
+
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 0; j <= 4; j++) {
+                a[i][j] = rd.nextInt(10);
+                System.out.print(a[i][j] + "\t");
+
+            }
+            System.out.println();
         }
+        System.out.println();
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 0; j <= 4; j++) {
+                System.out.print(a[j][i] + "\t");
+
+            }
+            System.out.println();
+        }
+
     }
+}
